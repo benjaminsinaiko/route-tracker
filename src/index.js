50e51097831fdbe78432ee1eda67eddf8f4de148
benjaminsinaiko <ben.jamin@outlook.com>
2020-02-26 10:46:43 -0600

    Add mongodb connection with mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const { mongoDb_username, mongoDb_password } = require('../keys');
 
 const app = express();
+
+const mongoUri = `mongodb+srv://${mongoDb_username}:${mongoDb_password}@cluster0-podpp.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', err => {
+  console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
