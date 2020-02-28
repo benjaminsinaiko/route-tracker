ef7c0afa40822daad98b9ba2c5678327707f2245
benjaminsinaiko <ben.jamin@outlook.com>
2020-02-27 22:21:10 -0600

    Add text inputs to signup screen

--- route-tracker-client/src/screens/SignupScreen.js
+++ route-tracker-client/src/screens/SignupScreen.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Headline, Text, TextInput, Button } from 'react-native-paper';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>SignupScreen</Text>
-      <Button title='Go to Signin' onPress={() => navigation.navigate('Signin')} />
+    <View style={styles.container}>
+      <Spacer>
+        <Headline>
+          Sign Up for <Text style={styles.name}>Route Tracker</Text>
+        </Headline>
+      </Spacer>
+      <TextInput
+        label='Email'
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize='none'
+        autoCorrect={false}
+        keyboardType='email-address'
+        textContentType='emailAddress'
+      />
+      <Spacer />
+      <TextInput
+        label='Password'
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize='none'
+        autoCorrect={false}
+        secureTextEntry
+        textContentType='newPassword'
+      />
+      <Spacer>
+        <Button mode='contained'>Sign Up</Button>
+      </Spacer>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  // style
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  },
+  name: {
+    color: '#6200ee'
+  }
 });
 
 export default SignupScreen;
