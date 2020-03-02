1131bd438740fffaf763b8726ceade8dd0265eeb
benjaminsinaiko <ben.jamin@outlook.com>
2020-03-02 00:37:33 -0600

    Save tracks, reset form, navigate to tracks

--- route-tracker-client/src/hooks/useSaveTrack.js
+++ route-tracker-client/src/hooks/useSaveTrack.js
@@ -1,15 +1,20 @@
 import { useContext } from 'react';
 import { TrackContext } from '../context/TrackContext';
 import { LocationContext } from '../context/LocationContext';
+import { useNavigation } from '@react-navigation/native';
 
 export default () => {
   const { createTrack } = useContext(TrackContext);
   const {
-    state: { locations, name }
+    state: { locations, name },
+    reset
   } = useContext(LocationContext);
+  const navigation = useNavigation();
 
-  const saveTrack = () => {
-    createTrack(name, locations);
+  const saveTrack = async () => {
+    await createTrack(name, locations);
+    await reset();
+    navigation.navigate('Tracks');
   };
 
   return [saveTrack];
