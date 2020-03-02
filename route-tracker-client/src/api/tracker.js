35c7ba4d59d9e7b62bdc572d603b6008ebe8aa63
benjaminsinaiko <ben.jamin@outlook.com>
2020-03-02 00:20:20 -0600

    Add token to header in axios instance

--- route-tracker-client/src/api/tracker.js
+++ route-tracker-client/src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
-  baseURL: 'http://f2edda71.ngrok.io' // will expire every 8hrs
+const instance = axios.create({
+  baseURL: 'http://4e4eac42.ngrok.io' // will expire every 8hrs
 });
+
+instance.interceptors.request.use(
+  async config => {
+    const userToken = await AsyncStorage.getItem('userToken');
+    if (userToken) {
+      config.headers.Authorization = `Bearer ${userToken}`;
+    }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
